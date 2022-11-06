c07754f3dac9a70639db537677867aa099e8e431
blgsn <111683241+blgsn@users.noreply.github.com>
2022-11-06 21:45:12 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
-
-
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		
      int total=0;
+     //asks the user for a value
 	 Scanner input = new Scanner(System.in);
      System.out.print("Bir sayı giriniz:");
      int number = input.nextInt();
-    
+    //the sum of the numbers dividing up to itself without a remainder
      for(int i=1;i<number;i++) {
     
          if (number%i==0) {
@@ -17,6 +16,7 @@ public class Main {
            total+=divider;
          }  
      }
+     //it is a perfect number if the sum is equal to itself
         if(total==number) {
         	 System.out.println(number+" Mükemmel bir sayıdır.");
          }
@@ -26,4 +26,6 @@ public class Main {
 	 } 
 	 
 }
+
+
 
